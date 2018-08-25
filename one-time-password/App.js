26ed636437e883d76c1b8781295fd13b186f9dec
Lucas Spacil <lucasspacil@Lucass-MacBook-Air.local>
2018-08-25 07:42:21 -0700

    removed keys

--- one-time-password/App.js
+++ one-time-password/App.js
@@ -3,18 +3,19 @@ import { StyleSheet, Text, View } from 'react-native';
 import firebase from 'firebase';
 import SignUpForm from './components/SignUpForm';
 import SignInForm from './components/SignInForm';
+require('dotenv').config();
 
 export default class App extends React.Component {
 
   componentDidMount() {
     // Initialize Firebase
     let config = {
-      apiKey: "AIzaSyBYC_OwnH8v-5d7dd3eCFBS4-NosJD0TYA",
-      authDomain: "one-time-password-ab3e3.firebaseapp.com",
-      databaseURL: "https://one-time-password-ab3e3.firebaseio.com",
-      projectId: "one-time-password-ab3e3",
-      storageBucket: "one-time-password-ab3e3.appspot.com",
-      messagingSenderId: "915340775511"
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID
     };
     firebase.initializeApp(config);
   }
